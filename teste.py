548750b89043035db9f900fc87745c8787a6233f
gomes-ti23 <124213806+gomes-ti23@users.noreply.github.com>
2023-02-01 21:53:00 -0300

    Aula_01

---
+++ teste.py
@@ -0,0 +1,18 @@
+'''print('testando o vscode')
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+n3 = float(input('Digite a terceita nota: '))
+resultado = (n1 + n2 + n3) / 3
+print('Sua média é: {:.2f}'.format(resultado))'''
+
+a = 2.35
+b = 3.15
+s = a + b
+print('soma =',s)
+print(type(s))
+
+a = '2'
+b = '3'
+s = a + b
+print('soma =',s)
+print(type(s))
